4e154bfb064a2c7628bd258dd5c8501dee15ac89
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2022-12-21 11:49:21 -0500

    updated at 2022-12-21T11:49:21-05:00

--- ext/app/server/lib/create.ts
+++ ext/app/server/lib/create.ts
@@ -2,6 +2,8 @@ import { Activation } from 'app/gen-server/lib/Activation';
 import { configureSendGridNotifier } from 'app/gen-server/lib/configureSendGridNotifier';
 import { checkAzureExternalStorage,
          configureAzureExternalStorage } from 'app/server/lib/configureAzureExternalStorage';
+import { checkMinIOExternalStorage,
+         configureMinIOExternalStorage } from 'app/server/lib/configureMinIOExternalStorage';
 import { checkS3ExternalStorage,
          configureS3ExternalStorage } from 'app/server/lib/configureS3ExternalStorage';
 import { makeSimpleCreator } from 'app/server/lib/ICreate';
@@ -15,6 +17,10 @@ export const create = makeSimpleCreator({
     {
       check: () => checkS3ExternalStorage() !== undefined,
       create: configureS3ExternalStorage,
+    },
+    {
+      check: () => checkMinIOExternalStorage() !== undefined,
+      create: configureMinIOExternalStorage,
     },
   ],
   billing: {
