6f005de9f82398f87e6797a67cfae54e7e61811f
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2025-02-17 16:11:50 -0500

    simplify sync logic

--- scripts/fetch_ext.sh
+++ scripts/fetch_ext.sh
@@ -21,25 +21,10 @@ workdir=tmp_checkout
 # Make a clean copy of ext directory by brute force.
 rm -rf $workdir
 git clone $monorepo $workdir
-# It is nice to keep core and ext code in sync. Let's look at the latest
-# commit in core, then try to find where it lives in monorepo. This will
-# fail if the latest commit in core didn't in fact come from monorepo.
-pushd core
-core_commit=$(git rev-parse HEAD)
-popd
-pushd $workdir
-git checkout $core_commit
-ext_commit=$(git log --pretty=format:"%H" --reverse --ancestry-path HEAD..origin/master | head -n1)
-git log --oneline --reverse --ancestry-path HEAD..origin/master
-git log --oneline --reverse --ancestry-path HEAD..origin/master | head -n1 | grep "Split .core/. into commit .$core_commit." || {
-  echo "Could not find ext commit to go with core"
-  exit 1
-}
-git checkout $ext_commit
-popd
+# There used to be complicated logic here to match commits between
+# core and ext, but it isn't practical. Better to just run this script
+# in the correct order in a sync.
 rm -rf ext
 cp -r $workdir/ext ext
 git add --all ext
 rm -rf $workdir
-echo "core commit is $core_commit"
-echo "ext commit is $ext_commit"
