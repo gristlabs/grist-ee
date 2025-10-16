3c6fbc46ff97b62e5a184955060ee88fb71b7f9f
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2025-10-16 11:03:28 -0400

    updated at 2025-10-16T11:03:28-04:00

--- ext/app/server/lib/InstallAdminUsingOrg.ts
+++ ext/app/server/lib/InstallAdminUsingOrg.ts
@@ -1,11 +1,12 @@
+import {ApiError} from 'app/common/ApiError';
+import {MapWithTTL} from 'app/common/AsyncCreate';
+import {getSetMapValue} from 'app/common/gutil';
+import * as roles from 'app/common/roles';
 import {User} from 'app/gen-server/entity/User';
 import {HomeDBManager} from 'app/gen-server/lib/homedb/HomeDBManager';
 import {appSettings} from 'app/server/lib/AppSettings';
 import {InstallAdmin, SimpleInstallAdmin} from 'app/server/lib/InstallAdmin';
 import log from 'app/server/lib/log';
-import {MapWithTTL} from 'app/common/AsyncCreate';
-import {getSetMapValue} from 'app/common/gutil';
-import * as roles from 'app/common/roles';
 
 // This implementation of InstallAdmin considers any user who is an owner of the org named by
 // GRIST_INSTALL_ADMIN_ORG env var to be an installation admin. The named org must already
@@ -31,31 +32,56 @@ export async function createInstallAdminUsingOrg(dbManager: HomeDBManager) {
 
 const CACHE_TTL_MS = Number(process.env.GRIST_INSTALL_ADMIN_ORG_CACHE_TTL_MS) || 30_000;
 
+interface AdminCache {
+  ids: Set<number>;
+  first?: User;
+}
+
 class InstallAdminUsingOrg extends InstallAdmin {
   // To avoid a trip to the DB for each page load, we cache the set of owners (as user IDs),
   // with a TTL, so that we re-check every 30 seconds.
-  private _cachedAdmins = new MapWithTTL<true, Promise<Set<number>>>(CACHE_TTL_MS);
+  private _cachedAdmins = new MapWithTTL<true, Promise<AdminCache>>(CACHE_TTL_MS);
 
   constructor(private _dbManager: HomeDBManager, public readonly installAdminOrg: string) {
     super();
   }
 
   public override async isAdminUser(user: User): Promise<boolean> {
-    return (await this._getSetOfAdmins()).has(user.id);
+    return (await this._getSetOfAdmins()).ids.has(user.id);
   }
 
-  private _getSetOfAdmins(): Promise<Set<number>> {
+  public override async getAdminUser(): Promise<User> {
+    const adminCache = await this._getSetOfAdmins();
+    const user = adminCache.first;
+    if (!user) {
+      throw new ApiError('Admin user not available', 500);
+    }
+    return user;
+  }
+
+  public override clearCaches(): void {
+    this._cachedAdmins.clear();
+  }
+
+  private _getSetOfAdmins(): Promise<AdminCache> {
     return getSetMapValue(this._cachedAdmins, true, () => this._fetchSetOfAdmins());
   }
 
-  private async _fetchSetOfAdmins(): Promise<Set<number>> {
+  private async _fetchSetOfAdmins(): Promise<AdminCache> {
     const scope = {userId: this._dbManager.getPreviewerUserId()};
     const members = this._dbManager.unwrapQueryResult(
       await this._dbManager.getOrgAccess(scope, this.installAdminOrg));
+    const ids = members.users
+      .filter(u => (u.access === roles.OWNER))
+      .map(u => u.id);
+
+    // Pick an admin user in a stable way (smallest id).
+    const firstId = ids.length > 0 ? Math.min(...ids) : undefined;
 
     // Installation admins are users with OWNER access to the special org.
-    return new Set(members.users
-      .filter(u => (u.access === roles.OWNER))
-      .map(u => u.id));
+    return {
+      ids: new Set(ids),
+      first: firstId ? await this._dbManager.getUser(firstId) : undefined,
+    };
   }
 }
